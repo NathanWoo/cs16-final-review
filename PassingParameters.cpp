23285aa8d20e799a23f89b5d0d0ed98ac7ebde86
NathanWoo <nathanwuyang@gmail.com>
2019-12-05 14:37:59 -0800

    nw - added content for PassingParameters

--- PassingParameters.cpp
+++ PassingParameters.cpp
@@ -15,11 +15,24 @@ void swapByReference(int &a, int &b){
 	b = temp;
 }
 
-void swapByAddress(int* &a, int* &b){
+void swapByAddress(int* a, int* b){
 	int temp = *a;
 	*a = *b;
 	*b = temp;
 }
+
+// This code is from https://stackoverflow.com/a/5789842/9706459 
+void pass_by_value(int* p) { 
+    //Allocate memory for int and store the address in p 
+    p = new int; 
+} 
+
+// This code is from https://stackoverflow.com/a/5789842/9706459 
+void pass_by_reference(int*& p) { 
+     p = new int; 
+} 
+
+
 
 int main() {
 	// passing by value, passing by reference and passing by address
@@ -41,6 +54,16 @@ int main() {
 	cout << "Before swapping, five = " << five << " six = " << six << endl;
 	swapByAddress(&five,&six);
 	cout << "After swapping by address, five = " << five << " six = " << six << endl;
+
+
+	int* p1 = NULL;
+    int* p2 = NULL;
+	cout << "Before passing in pass_by_value, p1 = " << p1 << endl;
+	cout << "Before passing in pass_by_reference, p2 = " << p2 << endl;
+	pass_by_value(p1); //p1 will still be NULL after this call 
+    pass_by_reference(p2); //p2 's value is changed to point to the newly allocate memory 
+	cout << "After passing in pass_by_value, p1 = " << p1 << endl;
+	cout << "After passing in pass_by_reference, p2 = " << p2 << endl;
 
 	return 0;
 }
